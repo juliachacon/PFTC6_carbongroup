c37f81655e352b4777b61f96bd79abe496fbbc91
juliachacon <juliachacon@gmail.com>
2022-07-31 21:45:01 +0200

    comments on the vizz of co2 values, and commented somewhere

--- code/vikesland/cleaning_vikesland.R
+++ code/vikesland/cleaning_vikesland.R
@@ -2,6 +2,7 @@
 # This script will be to separate c-flux data into turfIDs and clean the fluxes before we calculate them
 
 source("code/functions.R")
+
 library("dataDownloader")
 
 # download raw data
@@ -70,15 +71,15 @@ co2_cut_vikesland_60 <- co2_cut_vikesland_60 %>%
 
 # visualizing 60 secs cuts in Vikesland (it´s in comments, just in case you don´t want to visualize it)
 
-theme_set(theme_grey(base_size = 5))
-
-co2_cut_vikesland_60 %>%
-  ggplot(aes(x = datetime, y = CO2, colour = cut)) +
-  geom_line(size = 0.2, aes(group = fluxID)) +
-  # geom_line(size = 0.2) +
-  scale_x_datetime(date_breaks = "1 min", minor_breaks = "10 sec", date_labels = "%e/%m \n %H:%M") +
-  # scale_x_date(date_labels = "%H:%M:%S") +
-  facet_wrap(vars(fluxID), ncol = 30, scales = "free")
+# theme_set(theme_grey(base_size = 5))
+# 
+# co2_cut_vikesland_60 %>%
+#   ggplot(aes(x = datetime, y = CO2, colour = cut)) +
+#   geom_line(size = 0.2, aes(group = fluxID)) +
+#   # geom_line(size = 0.2) +
+#   scale_x_datetime(date_breaks = "1 min", minor_breaks = "10 sec", date_labels = "%e/%m \n %H:%M") +
+#   # scale_x_date(date_labels = "%H:%M:%S") +
+#   facet_wrap(vars(fluxID), ncol = 30, scales = "free")
 
 # ggsave("fluxes_details_vikesland.png", height = 40, width = 80, units = "cm")
 
@@ -104,7 +105,8 @@ plot(filt_ER_60$PAR) # Plot the PAR values
 plot(x= filt_ER_60$datetime, y= filt_ER_60$PAR) # Plot the PAR vs time
 abline(h=0, col="red")
 
-# now we are 
+# now we are replacing negative PAR values in type=ER by zero values.
+
 co2_cut_60_keep <- co2_cut_60_keep %>% 
   mutate(
     PAR =
@@ -114,6 +116,12 @@ co2_cut_60_keep <- co2_cut_60_keep %>%
     )
   )
 
+# let´s plot the PAR values for ER again:
+
+co2_cut_60_keep %>% 
+  filter(type=="ER") %>% 
+  ggplot(aes(x=datetime, y = PAR)) +
+  geom_point()
 
 #unique(filt_ER_60[filt_ER_60$PAR > 60,]$fluxID) # identify the weird values 
 #range(filt_ER_60[filt_ER_60$PAR > 60,]$PAR) # and the PAR levels (no big deal)
