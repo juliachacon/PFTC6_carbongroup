53cd134a1e0e15990ff79949f3db6a8d0f66ddfe
mukhlishholle <holle.mukhlish@gmail.com>
2022-08-01 09:33:03 +0200

    reading cflux vikesland dataset from OSF

--- code/data_dic/download_read_clean_data.R
+++ code/data_dic/download_read_clean_data.R
@@ -13,3 +13,11 @@ metadata <- read_csv("clean_data/Three-D_metaturfID.csv") %>%
   select(warming, turfID)
 
 
+# CFlux data -------------------------------------------------------------
+
+get_file(node = "pk4bg",
+          file = "Three-D_24h-cflux_vikesland_2022.csv",
+          path = "clean_data",
+          remote_path = "C-Flux")
+
+cflux_vikesland<-read.csv("clean_data/Three-D_24h-cflux_vikesland_2022.csv")
