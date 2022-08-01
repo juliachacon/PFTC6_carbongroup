b0fad794b7ea836a30de61613d1c2bd280621eb6
Joseph Gaudard <joseph.gaudard@uib.no>
2022-08-01 15:29:00 +0200

    str replace with better names

--- code/metaturf.R
+++ code/metaturf.R
@@ -6,4 +6,19 @@ get_file(node = "pk4bg",
          path = "raw_data",
          remote_path = "Site")
 
-metaturf <- read_csv("raw_data/Three-D_metaturfID.csv")
+metaturf <- read_csv("raw_data/Three-D_metaturfID.csv") %>% 
+  select(warming, origSiteID, turfID) %>% 
+  rename(
+    Site = origSiteID,
+    Warming = warming
+  ) %>% 
+  mutate(
+    Site = str_replace_all(
+      Site,
+      c("Lia" = "Liahovden" , "Joa" = "Joasete", "Vik" = "Vikesland")
+    ),
+    Warming = str_replace_all(
+      Warming,
+      c("W" = "Transplant", "A" = "Ambient")
+    )
+  )
