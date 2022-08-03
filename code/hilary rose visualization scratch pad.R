5e46a32c7c629af973581f13440a4c069f50688b
Hilary Rose Dawson <hrosedawson@gmail.com>
2022-08-03 14:50:15 +0200

    Cleaned graph with PAR and fluxes

--- code/hilary rose visualization scratch pad.R
+++ code/hilary rose visualization scratch pad.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(lubridate)
+library(hms)
+library(patchwork)
 
 # Merge data from all sites ----
 
@@ -34,19 +36,58 @@ ggplot(cflux_all %>% filter(type == "GPP"), aes(y = flux, x = PARavg, color = de
   theme_bw()
 
 # Scratch plot for time ----
-ggplot(cflux_all %>% filter(destSiteID == "Vik"), 
-       aes(y = flux, x = datetime, color = warming)) +
-  geom_line(inherit.aes = FALSE, aes(x = datetime, y = log(PARavg)), 
-            color = "goldenrod1") +
-  geom_area(inherit.aes = FALSE, aes(x = datetime, y = log(PARavg)), 
-            fill = "goldenrod1", alpha = 0.5) +
+hog.plot.er = 
+  ggplot(cflux_all %>% filter(origSiteID == "Hog") %>% filter(type == "ER"), 
+       aes(y = flux, x = time, color = warming)) +
   geom_point() +
   geom_smooth() +
   scale_color_manual(values = c("dodgerblue4", "firebrick4")) +
   facet_grid(type ~.) +
-  theme_bw()
+  ylim(-70, 80) +
+  theme_bw() +
+  theme(strip.background = element_blank(),
+        strip.text.y = element_blank())+
+  labs(title = "Ecosystem respiration (ER)", x = "Time") 
 
-png("visualizations/flux_PAR_Vik.png", res = 300, units = "in", width = 10, height = 8)
+hog.plot.gpp = 
+  ggplot(cflux_all %>% filter(origSiteID == "Hog") %>% filter(type == "GPP"), 
+         aes(y = flux, x = time, color = warming)) +
+  # geom_line(inherit.aes = FALSE, aes(x = time, y = log(PARavg)), 
+  #           color = "goldenrod1") +
+  # geom_area(inherit.aes = FALSE, aes(x = time, y = log(PARavg)), 
+  #           fill = "goldenrod1", alpha = 0.5) +
+  geom_point() +
+  geom_smooth() +
+  scale_color_manual(values = c("dodgerblue4", "firebrick4")) +
+  facet_grid(type ~.) +
+  theme_bw() +
+  ylim(-70, 80) +
+  theme(strip.background = element_blank(),
+        strip.text.y = element_blank()
+  )+
+  labs(title = "Gross primary productivity (GPP)", x = "") 
+
+hog.plot.par = 
+  ggplot(cflux_all %>% filter(destSiteID == "Hog") %>% filter(type == "GPP"), 
+         aes(y = PARavg, x = time)) +
+  geom_smooth(method = "loess", span = 1/3, color = "goldenrod1", se = FALSE) +
+  stat_smooth(
+      geom = 'area', method = 'loess', span = 1/3,
+      fill = "goldenrod1", alpha = 0.5) + 
+  geom_point(color = "goldenrod2") +
+  scale_color_manual(values = c("dodgerblue4", "firebrick4")) +
+  facet_grid(type ~.) +
+  theme_bw() +
+  scale_y_log10() +
+  theme(strip.background = element_blank(),
+        strip.text.y = element_blank()
+  )+
+  labs(title = "Reflectance (PAR)", x = "") 
+
+hog.plot.gpp + hog.plot.par + hog.plot.er +
+  plot_layout(ncol = 1, heights = c(2, 1, 2)) 
+
+png("visualizations/flux_PAR_Hog.png", res = 300, units = "in", width = 10, height = 10)
 dev.off()
 
 # Comparing origin and destination plot viz ----
