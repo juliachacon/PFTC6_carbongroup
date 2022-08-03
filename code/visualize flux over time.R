a09afcbb2781139e9d0ce0d4c950a01ae035b23e
Hilary Rose Dawson <hrosedawson@gmail.com>
2022-08-03 16:21:57 +0200

    Added code for each site

--- code/visualize flux over time.R
+++ code/visualize flux over time.R
@@ -28,7 +28,7 @@ colnames(cflux_all)
 # Scratch plot for time ----
 plot.flux.time = function(orig.site, flux.type, ylim1, ylim2, title) {
   
-  ggplot(cflux_all %>% filter(origSiteID == orig.site) %>% filter(type == flux.type), 
+  ggplot(cflux_all %>% filter(destSiteID == orig.site) %>% filter(type == flux.type), 
          aes(y = flux, x = time, color = warming)) +
   geom_point() +
   geom_smooth(method = "loess", span = 0.3) +
@@ -51,7 +51,6 @@ plot.par.time = function(dest.site) {
     fill = "goldenrod1", alpha = 0.5) + 
   geom_point(color = "goldenrod2") +
   scale_color_manual(values = c("dodgerblue4", "firebrick4")) +
-  facet_grid(type ~.) +
   theme_bw() +
   scale_y_log10() +
   theme(strip.background = element_blank(),
@@ -61,13 +60,46 @@ plot.par.time = function(dest.site) {
 }
 
 # Make the plots ----
+## Vikesland ----
+vik.plot.er = plot.flux.time("Vik", "ER", -70, 80, "Ecosystem respiration (ER)")
+vik.plot.gpp = plot.flux.time("Vik", "GPP", -70, 80, "Gross primary productivity (GPP)")
+vik.plot.par = plot.par.time("Vik")
+
+vik.plot.gpp + vik.plot.par + vik.plot.er +
+  plot_layout(ncol = 1, heights = c(2, 1, 2)) 
+
+png("visualizations/flux_PAR_Vik.png", res = 300, units = "in", width = 10, height = 10)
+dev.off()
+
+## Hogsete ----
 hog.plot.er = plot.flux.time("Hog", "ER", -70, 80, "Ecosystem respiration (ER)")
 hog.plot.gpp = plot.flux.time("Hog", "GPP", -70, 80, "Gross primary productivity (GPP)")
 hog.plot.par = plot.par.time("Hog")
-
 
 hog.plot.gpp + hog.plot.par + hog.plot.er +
   plot_layout(ncol = 1, heights = c(2, 1, 2)) 
 
 png("visualizations/flux_PAR_Hog.png", res = 300, units = "in", width = 10, height = 10)
+dev.off()
+
+## Joasete ----
+joa.plot.er = plot.flux.time("Joa", "ER", -70, 80, "Ecosystem respiration (ER)")
+joa.plot.gpp = plot.flux.time("Joa", "GPP", -70, 80, "Gross primary productivity (GPP)")
+joa.plot.par = plot.par.time("Joa")
+
+joa.plot.gpp + joa.plot.par + joa.plot.er +
+  plot_layout(ncol = 1, heights = c(2, 1, 2)) 
+
+png("visualizations/flux_PAR_Joa.png", res = 300, units = "in", width = 10, height = 10)
+dev.off()
+
+## Liahovden ----
+lia.plot.er = plot.flux.time("Lia", "ER", -70, 80, "Ecosystem respiration (ER)")
+lia.plot.gpp = plot.flux.time("Lia", "GPP", -70, 80, "Gross primary productivity (GPP)")
+lia.plot.par = plot.par.time("Lia")
+
+lia.plot.gpp + lia.plot.par + lia.plot.er +
+  plot_layout(ncol = 1, heights = c(2, 1, 2)) 
+
+png("visualizations/flux_PAR_Lia.png", res = 300, units = "in", width = 10, height = 10)
 dev.off()
