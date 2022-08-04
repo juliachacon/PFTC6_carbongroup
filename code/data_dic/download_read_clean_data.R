3422d67c398a1d78616b284a2083fdd55b9319ce
Joseph Gaudard <joseph.gaudard@uib.no>
2022-08-04 12:26:17 +0200

    changed names and added fluxes all together

--- code/data_dic/download_read_clean_data.R
+++ code/data_dic/download_read_clean_data.R
@@ -1,5 +1,6 @@
 library(dataDownloader)
 # library(tidyverse)
+source("code/metaturf.R")
 
 
 # meta turfID -------------------------------------------------------------
@@ -16,29 +17,33 @@ library(dataDownloader)
 # CFlux data -------------------------------------------------------------
 
 get_file(node = "fcbw4",
-         file = "Three-D_24h-cflux_vikesland_2022.csv",
+         file = "PFTC6_24h-cflux_vikesland_2022.csv",
          path = "clean_data",
          remote_path = "c_flux_data")
 
-cflux_vikesland <- read_csv("clean_data/Three-D_24h-cflux_vikesland_2022.csv", col_types = "ffdddTtd")
+cflux_vikesland <- read_csv("clean_data/PFTC6_24h-cflux_vikesland_2022.csv", col_types = "ffdddTtd")
 
 get_file(node = "fcbw4",
-         file = "Three-D_24h-cflux_hogsete_2022.csv",
+         file = "PFTC6_24h-cflux_hogsete_2022.csv",
          path = "clean_data",
          remote_path = "c_flux_data")
 
-cflux_hogsete <- read_csv("clean_data/Three-D_24h-cflux_hogsete_2022.csv", col_types = "ffdddTtd")
+cflux_hogsete <- read_csv("clean_data/PFTC6_24h-cflux_hogsete_2022.csv", col_types = "ffdddTtd")
 
 get_file(node = "fcbw4",
-         file = "Three-D_24h-cflux_liahovden_2022.csv",
+         file = "PFTC6_24h-cflux_liahovden_2022.csv",
          path = "clean_data",
          remote_path = "c_flux_data")
 
-cflux_liahovden <- read_csv("clean_data/Three-D_24h-cflux_liahovden_2022.csv", col_types = "ffdddTtd")
+cflux_liahovden <- read_csv("clean_data/PFTC6_24h-cflux_liahovden_2022.csv", col_types = "ffdddTtd")
 
 get_file(node = "fcbw4",
-         file = "Three-D_24h-cflux_joasete_2022.csv",
+         file = "PFTC6_24h-cflux_joasete_2022.csv",
          path = "clean_data",
          remote_path = "c_flux_data")
 
-cflux_joasete <- read_csv("clean_data/Three-D_24h-cflux_joasete_2022.csv", col_types = "ffdddTtd")
+cflux_joasete <- read_csv("clean_data/PFTC6_24h-cflux_joasete_2022.csv", col_types = "ffdddTtd")
+
+#all fluxes together
+cflux <- bind_rows(cflux_vikesland, cflux_hogsete, cflux_liahovden, cflux_joasete) %>% 
+  right_join(metaturf)
